b03ea22395b50e25dc244497aa0af9bf7b8d92ca
Bryan Liang <bryanliang7@gmail.com>
2017-10-14 10:05:54 -0400

    -Fixed the listview display

--- app/src/main/java/com/hackgt17/foodapp/MainActivity.java
+++ app/src/main/java/com/hackgt17/foodapp/MainActivity.java
@@ -8,13 +8,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> ingredients = new ArrayList<>(5);
+    private ArrayList<String> ingredients;
+    private ArrayAdapter adapter;
+    private ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //listview shit
+        mListView = (ListView) findViewById(R.id.list);
+        ingredients = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ingredients);
+        mListView.setAdapter(adapter);
+        adapter.setNotifyOnChange(true);
 
         FloatingActionButton camera = (FloatingActionButton) findViewById(R.id.cameraIcon);
         camera.setOnClickListener(new View.OnClickListener() {
@@ -48,8 +60,9 @@ public class MainActivity extends AppCompatActivity {
             case (0) : {
                 if (resultCode == Activity.RESULT_OK) {
                     String newIngredient = data.getStringExtra("Ingredient");
-                    // TODO: Make this add to a list view or cards
                     ingredients.add(newIngredient);
+                    //update listview
+                    ((ArrayAdapter)mListView.getAdapter()).notifyDataSetChanged();
                     Toast.makeText(this, "Added " + newIngredient, Toast.LENGTH_LONG).show();
                 }
                 break;
