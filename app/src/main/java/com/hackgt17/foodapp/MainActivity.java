a1825a465c85cfd5d42a0c61eade4d9f105a13d7
Bryan Liang <bryanliang7@gmail.com>
2017-10-15 01:17:57 -0400

    -Fixed padding on recipe view

--- app/src/main/java/com/hackgt17/foodapp/MainActivity.java
+++ app/src/main/java/com/hackgt17/foodapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hackgt17.foodapp;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,9 +15,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.speech.RecognizerIntent;
 import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.Toast;
+
 
 import com.github.ag.floatingactionmenu.OptionsFabLayout;
 import com.hackgt17.foodapp.models.Food2ForkSearchAPI;
@@ -26,6 +29,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 
 import static com.hackgt17.foodapp.R.id.parent;
 
@@ -80,9 +84,7 @@ public class MainActivity extends AppCompatActivity {
                         launchCameraActivity();
                         break;
                     case R.id.fab_text:
-                        Toast.makeText(getApplicationContext(),
-                                fabItem.getTitle() + "clicked!",
-                                Toast.LENGTH_SHORT).show();
+                        startVoiceInput();
                     default:
                         break;
                 }
@@ -139,6 +141,11 @@ public class MainActivity extends AppCompatActivity {
                     addIngredientToList(newIngredient);
                 }
                 break;
+            }
+            case (REQ_CODE_SPEECH_INPUT) : {
+                ArrayList<String> result = data
+                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                addIngredientToList(result.get(0));
             }
         }
     }
@@ -201,6 +208,24 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Toast.makeText(getApplicationContext(), "Sorry, but there was an error with your request.", Toast.LENGTH_LONG).show();
             }
+        }
+
+    }
+
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+
+    // Showing google speech input dialog
+
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+
         }
     }
 
